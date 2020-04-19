0b0a72c3e9d1535a90a61e6fe6f892057e80f2fa
WONDE <wondwosen.shi@gmail.com>
2020-04-19 15:24:43 +0200

    Home page layout edited

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,51 +1,20 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import Baground from "../img/blur-close-up-code.jpg";
 
 class Home extends Component {
   render() {
     return (
-      <div className='homePage'>
-        <img src={Baground} alt='backgroundIMage' style={homeStyle} />
-        <div style={homeTitle} className='homeTitle'>
-          <h4>Home Page ..</h4>
-          <Link
-            className='btn-small'
-            to='/product'
-            style={{ marginTop: "1rem" }}
-          >
+      <header className='hero'>
+        <div className='banner'>
+          <h1 className='banner-title'>React Redux E-Commerce ...</h1>
+
+          <Link to='/product' className='banner-btn'>
             {" "}
             Our Products
           </Link>
         </div>
-      </div>
+      </header>
     );
   }
 }
 export default Home;
-
-const homeStyle = {
-  maxHeight: "350px",
-  width: "100%",
-  objectFit: "cover",
-  position: "relative",
-  marginTop: "0",
-  opacity: "0.7",
-};
-const homeTitle = {
-  fontSize: "30rem",
-  position: "absolute",
-  top: "12rem",
-  right: "20rem",
-  color: "white",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  flexWrap: "wrap",
-  letterSpacing: "2rem",
-  background:
-    "linear-gradient(to right, rgba(20, 25, 255, 0.5), rgba(0, 20, 250, 0.3))",
-  padding: "2rem",
-  borderRadius: "2%",
-  zIndex: 4,
-};
