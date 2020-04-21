3c5664f0cf428aa426154d6536bbbb573b6b8149
WONDE <wondwosen.shi@gmail.com>
2020-04-21 02:38:15 +0200

    move materialize-ccs to index.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "materialize-css/dist/css/materialize.min.css";
 import "./App.css";
 import { connect } from "react-redux";
 
