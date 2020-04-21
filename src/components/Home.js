730efc95975a2959b01ca5af340e981b358f9314
WONDE <wondwosen.shi@gmail.com>
2020-04-21 03:53:52 +0200

    change backgroun imag from css to Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Background from "../img/blur-close-up-code.jpg";
 
 class Home extends Component {
   render() {
     return (
       <header className='hero'>
+        <img src={Background} alt='Background img' />
         <div className='banner'>
           <h1 className='banner-title'>React Redux E-Commerce ...</h1>
 
