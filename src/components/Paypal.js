bdbcb796258f26e3a9fff751b8d0b57b1bf13210
TsigeWonwosen <wondwosen.shi@gmail.com>
2025-04-22 19:39:11 +0200

    paypal amout bug updated.

--- src/components/Paypal.js
+++ src/components/Paypal.js
@@ -1,37 +1,35 @@
 import React from "react";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
-export default class MyApp extends React.Component {
-  render() {
-    const initialOptions = {
-      "client-id": process.env.REACT_APP_API_KEY,
-      currency: "USD",
-      intent: "capture",
-    };
-    return (
-      <PayPalScriptProvider options={initialOptions}>
-        <PayPalButtons
-          style={{ layout: "vertical" }}
-          createOrder={(data, actions) => {
-            return actions.order.create({
-              purchase_units: [
-                {
-                  amount: {
-                    value: "9.99", // update with your amount
-                  },
+export default function PayPal({ total, clearFromCart, history }) {
+  const initialOptions = {
+    "client-id": process.env.REACT_APP_API_KEY,
+    currency: "USD",
+    intent: "capture",
+  };
+  return (
+    <PayPalScriptProvider options={initialOptions}>
+      <PayPalButtons
+        style={{ layout: "vertical" }}
+        createOrder={(data, actions) => {
+          return actions.order.create({
+            purchase_units: [
+              {
+                amount: {
+                  value: total, // update with your amount
                 },
-              ],
-            });
-          }}
-          onApprove={(data, actions) => {
-            return actions.order.capture().then((details) => {
-              alert(
-                `Transaction completed by ${details.payer.name.given_name}`
-              );
-            });
-          }}
-        />
-      </PayPalScriptProvider>
-    );
-  }
+              },
+            ],
+          });
+        }}
+        onApprove={(data, actions) => {
+          return actions.order.capture().then((details) => {
+            clearFromCart();
+            history.push("/product");
+            alert(`Transaction completed by ${details.payer.name.given_name}`);
+          });
+        }}
+      />
+    </PayPalScriptProvider>
+  );
 }
